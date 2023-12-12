7303bac7ca6f1a6186c01862310e476ebf22ef6e
shoto <103328038+XTGamer1@users.noreply.github.com>
2023-12-13 02:25:36 +0530

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -15,7 +15,7 @@
         {% endif %}
     }
     else {
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Currently Offline <br> Join to Start</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Currently Offline </span>`;
         document.getElementById("serverIP").classList.remove("bg-green-900");
         document.getElementById("serverIP").classList.add("bg-orange-800");
 
